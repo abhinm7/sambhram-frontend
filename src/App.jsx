b57402ddc72a0577ec62423cb4b173c31280fefa
ABHIN M <abhin18m@gmail.com>
2024-10-25 10:33:52 +0530

    new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar/Navbar.jsx'
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Preloader from './Components/Preloader/Preloade.jsx'; // Import the Preloader component
+import { ContextProvider } from './Contexts/StoreContext.jsx';
 
-import Home from './Pages/Home/Home.jsx'
-import Registration from './Pages/Registration/Registration.jsx'
-import Event from './Pages/Events/Event.jsx'
 
-import { ContextProvider } from './Contexts/StoreContext.jsx'
+// Lazy load the pages
+const Home = lazy(() => import('./Pages/Home/Home.jsx'));
+import Registration from './Pages/Registration/Registration.jsx';  // Direct import
+import Event from './Pages/Events/Event.jsx';    
 
 function App() {
-
-  return (
-  
+  return (  
     <ContextProvider>
-
-    <Navbar/> 
+      <Navbar />
       <div className="app">
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Registration />} />
-          <Route path='/events' element={<Event />} />
-        </Routes>
-
+        {/* Suspense component displays the Preloader until the route component is ready */}
+        <Suspense fallback={<Preloader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Registration />} />
+            <Route path="/events" element={<Event />} />
+          </Routes>
+        </Suspense>
       </div>
-
     </ContextProvider>
-          
-    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Preloader/Preloade.jsx
@@ -0,0 +1,13 @@
+// Preloader.js
+import React from 'react';
+import './Preloader.css';  // Link to the spinner styles
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Preloader;

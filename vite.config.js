92db5fee1a624b027471b5104e40805fbabb1da4
ABHIN M <abhin18m@gmail.com>
2024-11-01 13:48:58 +0530

    conguration fallback

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,55 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
-import cssnano from 'cssnano'
-import {config} from 'dotenv'
+import cssnano from 'cssnano';
+import { config } from 'dotenv';
 
 config();
-
 
 export default defineConfig({
   plugins: [
     react(),
     compression({
-      algorithm: 'brotliCompress', // Use Brotli compression
+      algorithm: 'brotliCompress',
       ext: '.br',
     }),
   ],
   server: {
-    hmr: true, // Enable hot module replacement
-    middlewareMode: 'html',
-    historyApiFallback: true // Correct usage is actually via middleware, explained below
+    hmr: true, // Enables hot module replacement
+    fs: {
+      allow: ['.'],
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Adjust this to match your backend server
+        changeOrigin: true,
+      },
+    },
+    // Redirect all unknown routes to index.html for client-side routing
+    middlewareMode: true,
+    historyApiFallback: true,
+    rewrite: path => path.replace(/^\/[^.]+$/, '/index.html'),
   },
   build: {
     minify: 'terser',
     chunkSizeWarningLimit: 1500,
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks: {
-    //       'react-vendor': ['react', 'react-dom'],
-    //       'lodash-vendor': ['lodash'],  // For example, split lodash
-    //       // 'your-component': ['./src/components/YourComponent.jsx'], // Split large components
-    //     }
-    //   }
-    // },
     terserOptions: {
       compress: {
-        drop_console: true, // Remove console.logs in production
-        drop_debugger: true
-      }
-    }
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
-
   css: {
     postcss: {
       plugins: [
         cssnano({
-          preset: 'default', // Default minification options
+          preset: 'default',
         }),
       ],
     },
   },
-
-  define:{
-    'process.env':process.env
-  }
+  define: {
+    'process.env': process.env,
+  },
 });

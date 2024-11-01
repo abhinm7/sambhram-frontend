838e3be7e5f7869326670cab15aff2632d2b2da3
ABHIN M <abhin18m@gmail.com>
2024-11-01 13:52:29 +0530

    production mode edit

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,40 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
-import cssnano from 'cssnano';
-import { config } from 'dotenv';
+import cssnano from 'cssnano'
+import {config} from 'dotenv'
 
 config();
+
 
 export default defineConfig({
   plugins: [
     react(),
     compression({
-      algorithm: 'brotliCompress',
+      algorithm: 'brotliCompress', // Use Brotli compression
       ext: '.br',
     }),
   ],
-  server: {
-    hmr: true, // Enables hot module replacement
-    fs: {
-      allow: ['.'],
-    },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Adjust this to match your backend server
-        changeOrigin: true,
-      },
-    },
-    // Redirect all unknown routes to index.html for client-side routing
-    middlewareMode: true,
-    historyApiFallback: true,
-    rewrite: path => path.replace(/^\/[^.]+$/, '/index.html'),
-  },
   build: {
     minify: 'terser',
     chunkSizeWarningLimit: 1500,
     terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    
+    }
   },
+
   css: {
     postcss: {
       plugins: [
         cssnano({
-          preset: 'default',
+          preset: 'default', // Default minification options
         }),
       ],
     },
   },
-  define: {
-    'process.env': process.env,
-  },
+
+  define:{
+    'process.env':process.env
+  }
 });

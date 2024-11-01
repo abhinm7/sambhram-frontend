df5af4c8f6ecb349b1bff5b6e16c3b8f51716cdb
ABHIN M <abhin18m@gmail.com>
2024-11-01 14:14:17 +0530

    production optimization phase1

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,61 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
-import cssnano from 'cssnano';
-import { config } from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: 'brotliCompress', // Use Brotli compression
-      ext: '.br',
-    }),
-    {
-      name: 'single-page-app',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.method === 'GET' && !req.url.startsWith('/@') && !req.url.includes('.')) {
-            req.url = '/';
-          }
-          next();
-        });
-      },
-    },
-  ],
-  build: {
-    minify: 'terser',
-    chunkSizeWarningLimit: 1500,
-    terserOptions: {},
-  },
-  css: {
-    postcss: {
-      plugins: [
-        cssnano({
-          preset: 'default', // Default minification options
-        }),
-      ],
-    },
-  },
-  define: {
-    'process.env': process.env,
-  },
-});
+  plugins: [react()],
+})
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import compression from 'vite-plugin-compression';
+// import cssnano from 'cssnano';
+// import { config } from 'dotenv';
+
+// config();
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     compression({
+//       algorithm: 'brotliCompress', // Use Brotli compression
+//       ext: '.br',
+//     }),
+//     {
+//       name: 'single-page-app',
+//       configureServer(server) {
+//         server.middlewares.use((req, res, next) => {
+//           if (req.method === 'GET' && !req.url.startsWith('/@') && !req.url.includes('.')) {
+//             req.url = '/';
+//           }
+//           next();
+//         });
+//       },
+//     },
+//   ],
+//   build: {
+//     minify: 'terser',
+//     chunkSizeWarningLimit: 1500,
+//     terserOptions: {},
+//   },
+//   css: {
+//     postcss: {
+//       plugins: [
+//         cssnano({
+//           preset: 'default', // Default minification options
+//         }),
+//       ],
+//     },
+//   },
+//   define: {
+//     'process.env': process.env,
+//   },
+// });
+
+
+
+
+
+

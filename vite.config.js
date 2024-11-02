0884f0bdba4e1b985d74bc2d1c764585360089a5
ABHIN M <abhin18m@gmail.com>
2024-11-02 17:33:54 +0530

    default configs

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
-import cssnano from 'cssnano'
-import {config} from 'dotenv'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-config();
-
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: 'brotliCompress', // Use Brotli compression
-      ext: '.br',
-    }),
-  ],
-  build: {
-    minify: 'terser',
-    chunkSizeWarningLimit: 1500,
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks: {
-    //       'react-vendor': ['react', 'react-dom'],
-    //       'lodash-vendor': ['lodash'],  // For example, split lodash
-    //       // 'your-component': ['./src/components/YourComponent.jsx'], // Split large components
-    //     }
-    //   }
-    // },
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console.logs in production
-        drop_debugger: true
-      }
-    }
-  },
-
-  css: {
-    postcss: {
-      plugins: [
-        cssnano({
-          preset: 'default', // Default minification options
-        }),
-      ],
-    },
-  },
-
-  define:{
-    'process.env':process.env
-  }
-});
+  plugins: [react()],
+})

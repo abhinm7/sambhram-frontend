efad1d9b0103d494d46adaf11b8d22ba2acf4763
Abdulla Shafaz K U <115992034+shafaz9539@users.noreply.github.com>
2024-11-02 19:16:43 +0530

    Revert "Revert "Abhin""

--- src/Components/RegisterButton/RegisterButton.jsx
+++ src/Components/RegisterButton/RegisterButton.jsx
@@ -1,16 +1,25 @@
 import './RegisterButton.css'
 import React,{useContext} from 'react'
+import { useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../Contexts/StoreContext';
 
 const RegisterButton = ()=>{
-    const {selectedEvent} = useContext(StoreContext)
+    const navigate = useNavigate();
+    const {selectedEvent,setSelectedEvent} = useContext(StoreContext)
     const selectionLength = selectedEvent.length;
+
+    const handleCheckoutClick = () => {
+        navigate("/checkout"); 
+    };
     return(
-        <>
-        <div className="register-button">
-            <p>register</p>
+        <> 
+        <div className="register-button-container">
+        <div className="register-button" onClick={handleCheckoutClick}>
+            <p>Checkout</p>
              <p>{selectionLength}</p>
         </div>
+        </div>
+       
         </>
     )
 }

cd1069f87e79f07e55d85b76642c205727a149b5
ABHIN M <abhin18m@gmail.com>
2024-11-09 14:26:32 +0530

    minor issues

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
 
 const Header = () => {
+    const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+    const handleVideoLoad = () => {
+        setIsVideoLoaded(true); // Set the state to true when the video is ready
+    };
+
     return (
         <>
-
-
-
-
-            <div class="invite-container">
-                <video className='head-video'
+            <div className={`invite-container ${isVideoLoaded ? 'loaded' : 'loading'}`}>
+                <video 
+                    className='head-video'
                     autoPlay
                     loop
                     muted
                     playsInline
+                    onCanPlayThrough={handleVideoLoad} // Trigger when video can play through
                 >
                     <source src="header-comp.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
                 <div className="header-overlay"></div>
 
-                <div class="content">
-                    <h1 className="title-sd">
-                        Shree Devi
-                    </h1>
-
-                    <h1 class="title">SAMBHRAM'24</h1>
-
-                    <p class="description">
-                        We cordially invite you to our national level college fest of 2024
-                    </p>
-                    <p class="date">On Dec 06, 07 & 08</p>
-                    <button class="btn-white" href="#">
-                        Discover
-                    </button>
-                </div>
+                {isVideoLoaded && (
+                    <div className="content">
+                        <h1 className="title-sd">
+                            Shree Devi
+                        </h1>
+                        <h1 className="title">SAMBHRAM'24</h1>
+                        <p className="description">
+                            We cordially invite you to our national level college fest of 2024
+                        </p>
+                        <p className="date">On Dec 06, 07 & 08</p>
+                        <button className="btn-white" href="#">
+                            Discover
+                        </button>
+                    </div>
+                )}
             </div>
-
-
-
         </>
-    )
+    );
+};
 
-}
-
-export default Header
+export default Header;

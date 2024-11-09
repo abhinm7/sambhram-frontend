1e75a8aa9e816cd2eb4c302bbe8175c920ce47a8
ABHIN M <abhin18m@gmail.com>
2024-11-09 16:02:26 +0530

    success page

--- src/Pages/SuccessPage/SuccessPage.jsx
+++ src/Pages/SuccessPage/SuccessPage.jsx
@@ -1,4 +1,4 @@
-import './SuccessPage.css'
+import './SuccessPage.css';
 import React from 'react';
 
 const SuccessPage = () => {
@@ -6,18 +6,24 @@ const SuccessPage = () => {
         <>
             <div className="success-page">
                 <div className="success-component">
+                    
+                    <div className="flying-lantern">
+                        <img src="https://oriental-decor.com/wp-content/uploads/2023/10/2545-w-900x900.png" alt="Flying Lantern" />
+                    </div>
+                    <div className="flying-lantern">
+                        <img src="https://oriental-decor.com/wp-content/uploads/2023/10/2545-w-900x900.png" alt="Flying Lantern" />
+                    </div>
                     <div className="success-up">
-                        <img src="https://cdn2.iconfinder.com/data/icons/greenline/512/check-512.png" alt="" />
+                        <img src="https://cdn2.iconfinder.com/data/icons/greenline/512/check-512.png" alt="Checkmark" />
                         <h2>Registered successfully</h2>
                     </div>
-
                     <div className="success-down">
-                        head fahiz
+                        Head Fahiz
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default SuccessPage 
+export default SuccessPage;
